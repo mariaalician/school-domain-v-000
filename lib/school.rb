fe26324d6a657b4276309ffd98fdb7277c416354
Maria Nunez <mariaalician@gmail.com>
2018-07-04 02:28:35 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :name, :grade, :roster
+  
+
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  
+  
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    self.sort
+  end
+end
